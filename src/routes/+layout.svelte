31cad54d83c0935e3d2dd78f7bb69ca729d135cd
Bedir Aygun <Bediruna@hotmail.com>
2025-04-29 22:33:50 +0100

    made nav bar sticky

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,25 @@
 <script>
-    import Nav from '../components/Nav.svelte';
+    import Nav from "../components/Nav.svelte";
 </script>
 
-<div class="container my-5">
-    <Nav />
+<div class="container">
+    <div class="sticky-nav">
+        <Nav />
+    </div>
     <div class="row justify-content-center">
         <div class="col-lg-8 col-md-10">
             <slot></slot>
         </div>
     </div>
 </div>
+
+<style>
+    @media (min-width: 992px) {
+        .sticky-nav {
+            position: sticky;
+            top: 0;
+            z-index: 10;
+            background-color: var(--navbar-bg-color);
+        }
+    }
+</style>
