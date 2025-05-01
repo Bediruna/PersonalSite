2452c86861321751639aeb557579f0c76dfacb7d
Bedir Aygun <Bediruna@hotmail.com>
2025-05-01 12:44:45 +0100

    WIP: Displaying blog id

---
+++ src/routes/blog/[id]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { page } from '$app/stores';
+
+    // Subscribe to the $page store to access route parameters
+    $: id = $page.params.id;
+</script>
+
+<h1>Blog ID: {id}</h1>
