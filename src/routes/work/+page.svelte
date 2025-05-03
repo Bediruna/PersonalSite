c242b73d05528ae8f67179141c9cd18474d57126
Bedir Aygun <Bediruna@hotmail.com>
2025-05-03 21:40:44 +0100

    Improved work page styling

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -1,38 +1,42 @@
 <script>
-    import GitHubIcon from "../../components/icons/GitHubIcon.svelte";
-    import LinkIcon from "../../components/icons/LinkIcon.svelte";
-    export let data;
-    const { projects } = data;
+  import GitHubIcon from "../../components/icons/GitHubIcon.svelte";
+  import LinkIcon from "../../components/icons/LinkIcon.svelte";
+  export let data;
+  const { projects } = data;
 </script>
 
 <div class="container">
-    <h2 class="text-center mb-4">RECENT WORK</h2>
-    <div class="row">
-        {#each projects as project}
-            <div class="col-12 col-md-6 mb-4">
-                <div class="card p-4 border rounded shadow">
-                    <h3 class="text-xl font-semibold mb-2">{project.name}</h3>
-                    <p class="mb-3">{project.description}</p>
-                    <div class="d-flex align-items-center gap-3">
-                        <a
-                            href={project.html_url}
-                            target="_blank"
-                            class="text-primary text-decoration-none"
-                        >
-                            <GitHubIcon size={24} />
-                        </a>
-                        {#if project.homepage}
-                            <a
-                                href={project.homepage}
-                                target="_blank"
-                                class="text-decoration-none"
-                            >
-                                <LinkIcon size={24} />
-                            </a>
-                        {/if}
-                    </div>
-                </div>
-            </div>
-        {/each}
-    </div>
+  <h2 class="text-center mb-4">RECENT WORK</h2>
+  <div class="row">
+    {#each projects as project}
+      <div class="col-12 col-md-6 mb-4 d-flex">
+        <div
+          class="card p-4 border rounded shadow flex-grow-1 d-flex flex-column"
+        >
+          <h3 class="text-xl font-semibold mb-2">{project.name}</h3>
+          <p class="flex-grow-1">{project.description}</p>
+          <div class="d-flex justify-content-center gap-5">
+            <a
+              href={project.html_url}
+              target="_blank"
+              class="text-primary text-decoration-none"
+              title="View code on GitHub"
+            >
+              <GitHubIcon size={40} />
+            </a>
+            {#if project.homepage}
+              <a
+                href={project.homepage}
+                target="_blank"
+                class="text-decoration-none"
+                title="View website"
+              >
+                <LinkIcon size={40} />
+              </a>
+            {/if}
+          </div>
+        </div>
+      </div>
+    {/each}
+  </div>
 </div>
